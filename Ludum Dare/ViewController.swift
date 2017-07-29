ce90e0d26ddcfd6016c1ffeace7c6e0bd32a47e3
Nikita Ermolenko <nikita.ermolenko@rosberry.com>
2017-07-29 14:46:51 +0600

    Playing with light

--- Ludum Dare/ViewController.swift
+++ Ludum Dare/ViewController.swift
@@ -14,29 +14,67 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    fileprivate lazy var spotLightNode: SCNNode = {
+        let spotLight = SCNLight()
+        spotLight.type = .spot
+        spotLight.spotInnerAngle = 40
+        spotLight.spotOuterAngle = 45
+        spotLight.castsShadow = true
+        
+        let spotNode = SCNNode()
+        spotNode.light = spotLight
+        return spotNode
+    }()
+    
+    fileprivate lazy var spotLight: SCNLight = {
+        let spotLight = SCNLight()
+        spotLight.type = .spot
+        spotLight.spotInnerAngle = 0
+        spotLight.spotOuterAngle = 45
+        spotLight.castsShadow = true
+        return spotLight
+    }()
+    
+    var timer: Timer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Set the view's delegate
+
+//        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
         sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
+        sceneView.autoenablesDefaultLighting = false
+        sceneView.automaticallyUpdatesLighting = false
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
+        let scene = SCNScene(named: "art.scnassets/light_test.scn")!
         sceneView.scene = scene
+        sceneView.session.delegate = self
+        
+//        spotLightNode.position = SCNVector3(0, 0, -1)
+//        spotLightNode.eulerAngles = SCNVector3(-90, 0, 0)
+//        sceneView.scene.rootNode.addChildNode(spotLightNode)
+
+        timer = Timer(timeInterval: 0.2, repeats: true) { [unowned self] _ in
+            if let transform = self.sceneView.session.currentFrame?.camera.transform {
+                let cameraPosition = SCNVector3(transform.columns.3.x, transform.columns.3.y, transform.columns.3.z)
+                self.spotLightNode.position = SCNVector3(0, 0, cameraPosition.z)
+//                self.spotLightNode.worldPosition = SCNVector3(0, 0, cameraPosition.z)
+                
+//                print("camera position: \(SCNVector3(transform.columns.3.x, transform.columns.3.y, transform.columns.3.z))")
+            }
+        }
+//        RunLoop.current.add(timer, forMode: .commonModes)
+        
+        
+        
+//        sceneView.pointOfView?.addChildNode(spotLightNode)
+//        sceneView.pointOfView?.light = spotLight
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
-        // Create a session configuration
+
         let configuration = ARWorldTrackingSessionConfiguration()
-        
-        // Run the view's session
         sceneView.session.run(configuration)
     }
     
@@ -47,34 +85,18 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
+    // MARK: - Configurations
+}
 
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
+// MARK: - ARSessionDelegate
+
+extension ViewController: ARSessionDelegate {
+    func session(_ session: ARSession, didUpdate frame: ARFrame) {
+//        let transform = frame.camera.transform
+//        let cameraPosition = SCNVector3(transform.columns.3.x, transform.columns.3.y, transform.columns.3.z)
+//        spotLightNode.position = SCNVector3(0, 0, cameraPosition.z)
         
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
+        print(spotLightNode.position)
+        print(spotLightNode.worldPosition)
     }
 }
